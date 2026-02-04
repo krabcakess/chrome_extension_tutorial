224e0d59535583804604e9312cbe8f95319ff3de
Muhammad Izz Nabeel bin Muhamad Hafizi <s3986060@student.rmit.edu.au>
2026-02-04 14:16:00 +1100

    send video id from background to contentscript

--- background.js
+++ background.js
@@ -1,2 +1,18 @@
+// Chrome listener that runs fucntion whenever a tab is updated
+chrome.tabs.onUpdated.addListener((tabId, tab) => { // CHROME API
 
-  
+    // If tab has URL and includes "youtube.com/watch" phrase, run code inside
+    if (tab.url && tab.url.includes("youtube.com/watch")) {
+
+        // Get phrase after "?"
+        const queryParameters = tab.url.split("?")[1];
+        // Create a URLSearchParams object that eases URL parameter retrieval
+        const urlParameters = new URLSearchParams(queryParameters);
+
+        // Send message to contentScript
+        chrome.tabs.sendMessage(tabId, { // CHROME API 
+            type: "NEW",
+            videoId: urlParameters.get("v")
+        });
+    };
+});
